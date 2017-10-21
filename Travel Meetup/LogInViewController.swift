8d0a3caf411aaeb2bccd69878ca6fa705d14934e
Nulrybek Karshyga <nulrybekkarshyga@MacBook-Pro-Nulrybek.local>
2017-10-21 00:05:22 -0400

    New storyboard

---
+++ Travel Meetup/LogInViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LogInViewController.swift
+//  Travel Meetup
+//
+//  Created by Nulrybek Karshyga on 20.10.17.
+//  Copyright © 2017 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
