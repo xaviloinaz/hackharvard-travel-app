cfec5333538b44bb10581463c801d51c7d8fb807
Xavi Loinaz <x.e.loinaz@gmail.com>
2017-10-21 15:33:44 -0400

    made a passenger class. note: FirebaseApp.configure() line is commented out

---
+++ Travel Meetup/Passenger.swift
@@ -0,0 +1,33 @@
+//
+//  Passenger.swift
+//  Travel Meetup
+//
+//  Created by Xavi Loinaz on 10/21/17.
+//  Copyright © 2017 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class Passenger {
+
+    let name : String
+    let age : Int
+    let bio : String
+    let purposeOfTrip : String?
+    let lookingFor : String?
+    
+    
+    
+    init(name: String, age: Int, bio: String, purposeOfTrip: String?, lookingFor: String?) {
+        self.name = name
+        self.age = age
+        self.bio = bio
+        self.purposeOfTrip = purposeOfTrip
+        self.lookingFor = lookingFor
+    }
+    
+    
+    
+}
+
+
