a4409d7689f1d87130204c067d64a5ad31b967db
Nulrybek Karshyga <nulrybekkarshyga@MacBook-Pro-Nulrybek.local>
2017-10-21 01:00:12 -0400

    LogIn

--- Travel Meetup/SignupViewController.swift
+++ Travel Meetup/SignupViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SignupViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var secondPasswordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func createAccountAction(_ sender: UIButton) {
+        if passwordTextField.text == " " {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+        }
     }
     
     override func didReceiveMemoryWarning() {
