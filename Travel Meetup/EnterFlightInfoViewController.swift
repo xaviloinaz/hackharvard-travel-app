2788ba05c64f5eb7f8e91ddcd018188fa2dff52a
Xavi Loinaz <x.e.loinaz@gmail.com>
2017-10-20 23:20:44 -0400

    connected code files to corresponding view controllers

---
+++ Travel Meetup/EnterFlightInfoViewController.swift
@@ -0,0 +1,25 @@
+//
+//  EnterFlightInfoViewController.swift
+//  Travel Meetup
+//
+//  Created by Xavi Loinaz on 10/20/17.
+//  Copyright © 2017 Xavi Loinaz. All rights reserved.
+//
+
+import UIKit
+
+class EnterFlightInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
